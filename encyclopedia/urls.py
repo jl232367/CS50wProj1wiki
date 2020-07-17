23bcf033a817b5ce66d29a6d62dcb0d1e8aefcb5
Patrick <patches72790@gmail.com>
2020-07-17 15:20:59 -0500

    got links on homepage working

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 
 from . import views
 
-# app_name = "encyclopedia"
-
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:entry>", views.entry, name="entry")
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -7,15 +7,19 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def normalize_str(word):
+    first_letter = word[0].upper()
+    return first_letter + word[1:]
+
 def entry(request, entry):
     # if entry doesn't exist
     entry_list = util.list_entries()
-    if entry not in entry_list:
+    if normalize_str(entry) not in entry_list:
         return render(request, "encyclopedia/error.html", {
             "entry": entry
         })
     # else, return entry page
     return render(request, "encyclopedia/entry.html", {
-        "entry": entry,
+        "entry": normalize_str(entry),
         "text": util.get_entry(entry)
     })
